c5a4ebb80ba34454f3316a194554f74d1314bba5
Admiral-Fish <admiralfish420@gmail.com>
2019-09-04 15:44:25 -0700

    Only allow mods to reload cogs

--- admin.py
+++ admin.py
@@ -9,13 +9,13 @@ class Admin(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(kick_user=True)
-    async def kick(self, ctx, user: discord.Member, *reason):
+    async def kick(self, ctx, user: discord.Member, *, reason):
         await user.kick(reason=reason)        
         await self.bot.log_channel.send(f"{ctx.author.mention} kicked {user.mention} for reason: {reason}")
 
     @commands.command()
     @commands.has_permissions(ban_user=True)
-    async def ban(self, ctx, user: discord.Member, *reason):
+    async def ban(self, ctx, user: discord.Member, *, reason):
         await user.ban(reason=reason)        
         await self.bot.log_channel.send(f"{ctx.author.mention} banned {user.mention} for reason: {reason}")
 
@@ -26,17 +26,20 @@ class Admin(commands.Cog):
 
         await ctx.message.delete()
         await ctx.author.add_roles(self.bot.member_role)     
-        await self.bot.log_channel.send(f"Added member tole to {ctx.author.mention} | {ctx.author} | {ctx.author.id}")
+        await self.bot.log_channel.send(f"Added member role to {ctx.author.mention} | {ctx.author} | {ctx.author.id}")
 
     @commands.command()
     async def reload(self, ctx):
-        for cog in extensions.cogs:
-            try:
-                self.bot.unload_extension(cog)
-                self.bot.load_extension(cog)
-            except Exception as e:
-                await self.bot.log_channel.send(f"Failed to load addon: {cog} due to `{type(e).__name__}: {e}`")
-                print(f"Failed to load addon: {cog} due to `{type(e).__name__}: {e}`")
+        if self.bot.mod_role in ctx.author.roles:
+            for cog in extensions.cogs:
+                try:
+                    self.bot.unload_extension(cog)
+                    self.bot.load_extension(cog)
+                except Exception as e:
+                    await self.bot.log_channel.send(f"Failed to load addon: {cog} due to `{type(e).__name__}: {e}`")
+                    print(f"Failed to load addon: {cog} due to `{type(e).__name__}: {e}`")
+        else:
+            await self.bot.log_channel.send(f"{ctx.author.mention} attempted to reload the cogs")
 
 def setup(bot):
     bot.add_cog(Admin(bot))
