a0027004f04835baa5cb6be538f7b93fa1b90853
Admiral-Fish <admiralfish420@gmail.com>
2019-09-06 11:30:09 -0700

    Delete reload messages

--- cogs/botutil.py
+++ cogs/botutil.py
@@ -10,27 +10,27 @@ class BotUtil(commands.Cog):
         if self.bot.mod_role in ctx.author.roles:
             try:
                 self.bot.reload_extension(cog)
-                await self.bot.log_channel.send(f"`{cog}` reloaded successfully")
+                await self.bot.log_channel.send(f"`{cog}` reloaded successfully", delete_after=5)
             except Exception as e:
-                await self.bot.log_channel.send(f"Failed to reload addon: {cog} due to `{type(e).__name__}: {e}`")
+                await self.bot.log_channel.send(f"Failed to reload addon: {cog} due to `{type(e).__name__}: {e}`", delete_after=5)
 
     @commands.command(hidden=True)
     async def load(self, ctx, cog):
         if self.bot.mod_role in ctx.author.roles:
             try:
                 self.bot.load_extension(cog)
-                await self.bot.log_channel.send(f"`{cog}` loaded successfully")
+                await self.bot.log_channel.send(f"`{cog}` loaded successfully", delete_after=5)
             except Exception as e:
-                await self.bot.log_channel.send(f"Failed to load addon: {cog} due to `{type(e).__name__}: {e}`")
+                await self.bot.log_channel.send(f"Failed to load addon: {cog} due to `{type(e).__name__}: {e}`", delete_after=5)
 
     @commands.command(hidden=True)
     async def unload(self, ctx, cog):
         if self.bot.mod_role in ctx.author.roles:
             try:
                 self.bot.unload_extension(cog)
-                await self.bot.log_channel.send(f"`{cog}` unloaded successfully")
+                await self.bot.log_channel.send(f"`{cog}` unloaded successfully", delete_after=5)
             except Exception as e:
-                await self.bot.log_channel.send(f"Failed to unload addon: {cog} due to `{type(e).__name__}: {e}`")
+                await self.bot.log_channel.send(f"Failed to unload addon: {cog} due to `{type(e).__name__}: {e}`", delete_after=5)
 
 def setup(bot):
     bot.add_cog(BotUtil(bot))
